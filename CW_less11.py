3273e2c0444cf5ef4af22ee28c97225acd0e7530
Росохата Олександра <SashunRo@gmail.com>
2025-11-17 22:33:20 +0200

    Add files via upload

--- CW_less11.py
+++ CW_less11.py
@@ -1,60 +1,88 @@
-###1. Скачуємо бібліотеки
-#pip install tensorflow pandas numpy matplotlib scikit-learn
-import pandas as pd #працюємо з файлами csv таблицями
-import numpy as np #математичні операції
-import tensorflow as tf #Створює нейронку
-#keras - частина тф, яка створює шари в нейронці шар-група нейронів одного рівня
-from tensorflow import keras
-from tensorflow.keras import layers
-from sklearn.preprocessing import LabelEncoder #переводить назви в числа, для того щоб нейронка розуміла, що це саме ця фігура
-import matplotlib.pyplot as plt #для побудови візуалізації даних, графіків
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+X = []
+y = []
+
+colors = {
+    "red": (0, 0, 255),
+    "green": (0, 255, 0),
+    "blue": (255, 0, 0),
+    "yellow": (0, 255, 255),
+    "orange": (0, 128, 255),
+    "purple": (255, 0, 255),
+    "pink": (180, 105, 255),
+    "white": (255, 255, 255)
+}
 
 
-###2. Зчитуємо інформацію з csv таблиці
-df = pd.read_csv("data/figures.csv")#datafile
-# print(df.head())
+for color_name, bgr in colors.items():
+        for i in range(20):
+            noise = np.random.randint(-20, 20, 3)
+            sample = np.clip(np.array(bgr) + noise, 0, 255)
+            X.append(sample)
+            y.append(color_name)
+
+def shape_recognition(cnt):
+    perimeter = cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, 0.04 * perimeter, True)
+    corners = len(approx)
+
+    x, y, w, h = cv2.boundingRect(cnt)
+
+    aspect_ratio = w / h
+    if corners == 3:
+        return "Triangle"
+    elif corners == 4:
+        if 0.9 < aspect_ratio < 1.2:
+            return "Square"
+        if 3 > aspect_ratio > 1.4:
+            return "Rectangle"
+    else:
+        if corners > 7:
+            return "Circle"
+        else:
+            return "Unknown"
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify = y)
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train, y_train)
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not(ret):
+        break
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, (30, 30, 30), (225, 225, 225))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8))
 
 
-###3. Перетворюємо значення фігурам в числа
-encoder = LabelEncoder()
-df['label_enc'] = encoder.fit_transform(df['label']) #створюємо новий стовпець в таблиці. Значення - перетворений рядок label
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 3000:
+            x, y, w, h = cv2.boundingRect(cnt)
+            shape = shape_recognition(cnt)
+            roi = frame[y:y + h, x:x + w]
 
-X = df[['area', 'perimeter', 'corners']] #додаємо наші параметри в ознаки
-y = df[['label_enc']] #додаємо вже перетворені числа для фігур 20рядок) в мітки
-
-
-###4. Створення моделі
-#Послідовно з`єднанні шари, табличні дані - тип нейронки
-# model = keras.Sequential(layers.Dense(8, activation = "relu")) #Три аргументи - шари. 1-кількість нейронів(8) 2-активація (кортеж параметрів)
-model = keras.Sequential([
-    layers.Dense(8, activation = 'relu', input_shape = (3,)),
-    layers.Dense(8, activation = 'relu'),
-    layers.Dense(8, activation = 'softmax')
-])
+            mean_color = cv2.mean(roi)[:3]
+            mean_color_array = np.array(mean_color).reshape((1, -1))
+            color_label = model.predict(mean_color_array)[0]
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255,255,255), 2)
+            cv2.putText(frame, f"{color_label} {shape}", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
 
 
-###.5 Компаляція моделі (визначення навчання)
-model.compile(optimizer = 'adam', loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
-#1 - оптимізація, adam - визначає алгоритм для навчання, 2 -втрати, 3 - точність
+    cv2.imshow("color and shape", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 
-###.6 Навчання
-history = model.fit(X, y, epochs = 300, verbose = 0) #Останній аргумент необов`язковий. Не виводить аналіз
 
-#Графіки: 1 - втрата, 2 - точність
-plt.plot(history.history['loss'], label = "Втрати")
-plt.plot(history.history['accuracy'], label = "Точність")
-#Підписуємо вісь x та y
-plt.xlabel('Епоха')
-plt.ylabel('Значення')
-plt.title('Процес навчання моделі')
-plt.legend()
-plt.show()
-
-
-###.7 Тестування
-test = np.array(df[25, 20, 0])
-predict = model.predict(test) #Отримуємо ймовірність
-
-print(f'Імовірність кожного класу {predict}')
-print(f'Модель визначила {encoder.inverse_transform([np.argmax(predict)])}')
+cap.release()
+cv2.destroyAllWindows()
